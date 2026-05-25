3b412ec108fabc8d6556595c6c9ca76182ba6b15
Rukmini-S-Ashtankar <25ashtankarrs_1@rbunagpur.in>
2026-05-25 12:17:42 +0530

    Delete rem.py

--- rem.py
+++
@@ -1,7 +0,0 @@
-#Remove spaces from string:
-# 1. retrip()===> To remove spaces at right hand side
-# 2. lstrip()===> To remove spaces at the left side
-# 3. strip()===> To remove spaces from both sides
-
-city=input("Enter the city Name: ")
-scity=city.strip()
